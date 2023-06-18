8f1b5a4b79892b810b4eef554f77dbe8d3b063ce
taibislamdipu <mailtaibislam@gmail.com>
2023-06-19 05:09:00 +0600

    UI: initial ready.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import "./App.css";
-import Podcast from "./components/podcast/podcast";
+import demo from "./assets/demo.mp3";
+import thumbnail from "./assets/podcastThumbnail.png";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [podcasts, setPodcasts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/v1/podcast");
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+        const result = await response.json();
+        setPodcasts(result?.data);
+      } catch (error) {
+        console.log("Error:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        <Podcast />
-        <Podcast />
+        {podcasts?.map((podcast, i) => (
+          <div className="col" key={i}>
+            <div className="card h-100">
+              <img src={thumbnail} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{podcast?.title}</h5>
+                <p className="card-text">{podcast?.description}</p>
+                <div>
+                  <audio controls>
+                    <source src={demo} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/components/podcast/PodcastCard.jsx
+++ src/components/podcast/PodcastCard.jsx
@@ -1,10 +1,10 @@
-const Podcast = () => {
+const PodcastCard = () => {
   return (
     <div className="col">
       <div className="card h-100">
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">title</h5>
           <p className="card-text">
             This is a longer card with supporting text below as a natural
             lead-in to additional content. This content is a little bit longer.
@@ -15,4 +15,4 @@ const Podcast = () => {
   );
 };
 
-export default Podcast;
+export default PodcastCard;
